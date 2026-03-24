7aef23a7175b66a13eea7e3f166af15741beb928
Kevin Bourguignon <kevin@Kevins-MacBook-Air.local>
2026-03-24 15:43:01 +0100

    Fix homepage white screen by avoiding hydration mismatch

--- index.tsx
+++ index.tsx
@@ -1,6 +1,6 @@
 
 import React from 'react';
-import { hydrateRoot, createRoot } from 'react-dom/client';
+import { createRoot } from 'react-dom/client';
 import './app.css';
 import App from './App.tsx';
 
@@ -9,18 +9,11 @@ if (!rootElement) {
   throw new Error("Could not find root element to mount to");
 }
 
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(
-    rootElement,
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-} else {
-  const root = createRoot(rootElement);
-  root.render(
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  );
-}
+// Always do a client mount to avoid hydration mismatch crashes from prerendered HTML.
+rootElement.innerHTML = '';
+const root = createRoot(rootElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
